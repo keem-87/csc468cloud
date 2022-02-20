220d7a02cc9e79dffeaefd83980222058de0dcc3
Linh B Ngo <lngo@wcupa.edu>
2022-02-20 15:42:07 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -22,6 +22,10 @@ node = request.XenVM("docker")
 node.cores = 8
 node.ram = 8192
 node.routable_control_ip = "true" 
+
+bs_landing = node.Blockstore("bs_image", "/image")
+bs_landing.size = "500GB"
+  
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
 node.routable_control_ip = "true"
 node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
