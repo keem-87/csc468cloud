8108fb6b925b5c8b45fdda343b07d1707aeab3ea
Linh B Ngo <lngo@wcupa.edu>
2022-02-20 15:02:03 -0500

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent sof
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   
 sudo apt-get update
