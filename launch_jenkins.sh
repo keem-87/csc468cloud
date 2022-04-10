2be8f26010718ca5b21248b91ebc40099c4bf1bf
Linh B Ngo <lngo@wcupa.edu>
2022-04-10 15:26:28 -0400

    Update launch_jenkins.sh

--- launch_jenkins.sh
+++ launch_jenkins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo cp /etc/kubernetes/manifests/kube-apiserver.yaml /local/repository/kube-apiserver.yaml.backup
-sudo sed '/^    - --service-cluster-ip-range/a \ \ \ \ - --service-node-port-range=30000-50000' /etc/kubernetes/manifests/kube-apiserver.yaml > /etc/kubernetes/manifests/kube-apiserver.yaml
+sudo sed '/^    - --service-cluster-ip-range/a \ \ \ \ - --service-node-port-range=30000-50000' /etc/kubernetes/manifests/kube-apiserver.yaml | sudo tee /etc/kubernetes/manifests/kube-apiserver.yaml
 kubectl create namespace jenkins
 kubectl create clusterrolebinding permissive-binding --clusterrole=cluster-admin --user=admin --user=kubelet --group=system:serviceaccounts
 kubectl -n kube-system create sa jenkins
